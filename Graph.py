34e3939cfe1370e5126e2868f91aa7882032fb81
Shishir Choudhary <shishirchoudhary198@gmail.com>
2020-08-30 15:33:22 +0530

    Graph Representation using matrix

--- Graph.py
+++ Graph.py
@@ -10,9 +10,81 @@
 # if there are lot of edges than nodes - adjancy matrix
 # if there are very less edges  - adjancy list
 
-# Adjacency list
+# Using Adjacency Matrix
+class UndirectedGraphMatrix:
+	def __init__(self,n):
+		self.vertices = n
+		self.graph_rep =[[0]*n for i in range(n)]
+
+	def addVertices(self):
+		for i in range(self.vertices):
+			 existing_row= self.graph_rep[i]
+			 existing_row.append(0)
+
+		self.vertices = self.vertices+1
+		newrow = [0]*self.vertices
+		newgraph = self.graph_rep
+		newgraph.append(newrow)
+
+	def addEdge(self,node1,node2):
+		self.graph_rep[node1-1][node2-1] = 1
+		self.graph_rep[node2-1][node1-1] = 1
+
+	def showGraph(self):
+		n = self.vertices
+		for i in range(n):
+			for j in range(n):
+				print(self.graph_rep[i][j] , end='\t')
+			print()
 
 
+
+# g = UndirectedGraphMatrix(4)
+# g.addVertices()
+# # g.showGraph()
+# g.addEdge(1,2)
+# g.addEdge(5,3)
+# g.showGraph()
+
+
+
+
+class DirectedGraphMatrix:
+	def __init__(self,n):
+		self.vertices = n
+		self.graph_rep =[[0]*n for i in range(n)]
+
+	def addVertices(self):
+		for i in range(self.vertices):
+			 existing_row= self.graph_rep[i]
+			 existing_row.append(0)
+
+		self.vertices = self.vertices+1
+		newrow = [0]*self.vertices
+		newgraph = self.graph_rep
+		newgraph.append(newrow)
+
+	def addEdge(self,node1,node2):
+		self.graph_rep[node1-1][node2-1] = 1
+		
+
+	def showGraph(self):
+		n = self.vertices
+		for i in range(n):
+			for j in range(n):
+				print(self.graph_rep[i][j] , end='\t')
+			print()
+
+
+
+g = DirectedGraphMatrix(4)
+g.addVertices()
+# g.showGraph()
+g.addEdge(1,2)
+g.addEdge(5,3)
+g.showGraph()
+
+# Using Adjacency list
 class UndirectedGraphList:
 	
 	def __init__(self):
@@ -33,14 +105,14 @@ class UndirectedGraphList:
 	def showGraph(self):
 		print(self.graph_rep)
 
-g = UndirectedGraphList()
-g.addVertices('A')
-g.addVertices('B')
-g.addVertices('E')
-g.addEdge('A','E')
-g.addEdge('A','B')
-g.addEdge('B','E')
-g.showGraph()
+# g = UndirectedGraphList()
+# g.addVertices('A')
+# g.addVertices('B')
+# g.addVertices('E')
+# g.addEdge('A','E')
+# g.addEdge('A','B')
+# g.addEdge('B','E')
+# g.showGraph()
 
 class DirectedGraphList:
 	
@@ -61,14 +133,14 @@ class DirectedGraphList:
 		print(self.graph_rep)
 
 
-g = DirectedGraphList()
-g.addVertices('A')
-g.addVertices('B')
-g.addVertices('E')
-g.addEdge('A','E')
-g.addEdge('A','B')
-g.addEdge('B','E')
-g.showGraph()
+# g = DirectedGraphList()
+# g.addVertices('A')
+# g.addVertices('B')
+# g.addVertices('E')
+# g.addEdge('A','E')
+# g.addEdge('A','B')
+# g.addEdge('B','E')
+# g.showGraph()
 
 
 
